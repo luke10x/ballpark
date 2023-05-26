a4d7a208bf17ba2ffd7c24cae636e6da31440251
Luke 10X <spam@luke10x.dev>
2023-05-25 22:57:29 -0400

    yes this is not needed

--- src/gl.h
+++ src/gl.h
@@ -1,13 +1,3 @@
 #pragma once
 
-# Maybe I don't need this file at all?
-
-#ifdef __EMSCRIPTEN__
 #include <GL/glew.h>
-#else
-#ifdef __APPLE__
-#include <GL/glew.h>
-#else
-#include <GL/glew.h>
-#endif
-#endif
