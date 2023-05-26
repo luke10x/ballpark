e78e81d91a3fb840954fdb65f26b22b06c4674e2
Luke 10X <spam@luke10x.dev>
2023-05-25 22:55:08 -0400

    This effectivelly deprecates gl.h, don't it?

--- src/gl.h
+++ src/gl.h
@@ -1,10 +1,12 @@
 #pragma once
 
+# Maybe I don't need this file at all?
+
 #ifdef __EMSCRIPTEN__
 #include <GL/glew.h>
 #else
 #ifdef __APPLE__
-#include <OpenGL/gl3.h>
+#include <GL/glew.h>
 #else
 #include <GL/glew.h>
 #endif
